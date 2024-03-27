e8a2821a6f359c24ab8163ef24e6eae5973d6a49
Robel Equbasilassie <robiki4life@gmail.com>
2024-03-27 11:36:49 +0300

    fix: Revert deployment branch to defalut branch 'gh-pages'

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,7 +24,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "tech-acs", // Usually your GitHub org/user name.
   projectName: "pmd-docs", // Usually your repo name.
-  deploymentBranch: "deploy",
+  // deploymentBranch: "gh-pages",
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
